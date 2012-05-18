b03b32ff56f2036495a22c96560e4309e05c7f91
Matt Beckett <beck24@gmail.com>
2012-05-18 10:08:10 -0600

    Updated instructions, format of changelog

--- extendafriend/languages/en.php
+++ extendafriend/languages/en.php
@@ -2,13 +2,13 @@
 
 	$english = array(
 		'extendafriend:cancel' => "Cancel",
-		'extendafriend:edit:friend' => "Edit Friend",
-		'extendafriend:form:instructions' => "Enter tags separated by a comma, if a collection exists with a tag name the friend will be added to that collection. If a collection does not exist, one will be created.  If you have existing collections, they can be controlled using the appropriate checkboxes.",
+		'extendafriend:edit:friend' => "Collections",
+		'extendafriend:form:instructions' => "<strong>Collections</strong> are <em>optional</em> ways to group your friends. You can use them to limit access to content you create, and to filter things you see and notifications you receive. No one else will be able to see your collections: they are completely private to you.<br /><p><strong>To create or edit collections</strong>, enter names for them separated by commas (e.g. <em>friends, colleagues, students, tutors</em>). If you have any existing collections, they will appear with checkboxes beside them: checking or unchecking a box will add or remove this person from that collection. When you have finished editing your collections, press Return or click the Submit button. </p><br /><p> To add this person as a friend without adding them to any collections, just click the submit button. If you don't want to friend this person, <em>close this popup</em></p>",
 		'extendafriend:invalid:id' => "Invalid ID",
 		'extendafriend:name' => "Extendafriend",
-		'extendafriend:rtags' => "Relationship Tags (optional)",
+		'extendafriend:rtags' => "Collections (optional)",
 		'extendafriend:submit' => "Submit",
-		'extendafriend:updated' => "Friend access collections have been updated",
+		'extendafriend:updated' => "Your collections have been updated",
 	);
 					
 	add_translation("en",$english);
